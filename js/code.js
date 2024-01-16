b8dbe4f6b7265f9773e43a7f950c780c00ab5a40
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2024-01-16 12:32:04 +0200

    Create function that Selects the marker of an active player

--- js/code.js
+++ js/code.js
@@ -1,5 +1,5 @@
 //gameboard object:
-function gameBoard(){
+const gameBoard = (function (){
     //create number of rows
     let rows = 3;
     //create number of columns
@@ -8,6 +8,7 @@ function gameBoard(){
     let board = [];
     //create the gameboard
     generateGameBoard();
+    
     //function to genearte game board
     function generateGameBoard(){
         for(let row = 0; row < rows;row ++){
@@ -18,9 +19,18 @@ function gameBoard(){
         }
 
     }
-    //getMark:
+    //getMarker:
+    function getMarker(){
+        //call game controller module and return active player
+        let activePlayerMarker = gameController().selectActivePlayer();
+        return activePlayerMarker;
+    
+
+    }
     //setMark(row,column,mark):
-    function setMark(row,column,mark){
+    function setMarker(row,column){
+        //get active players marker
+        
         //create variable for empty cell
         const emptyCell = "";
         //create variable for cell
@@ -28,30 +38,64 @@ function gameBoard(){
         //check if selected cell is empty
         if(cell === emptyCell){
             //set the player mark to a particular selected cell
-            board[row][column]= mark;
+            board[row][column]= getMarker();
         }
 
     }
-    return{setMark,board};
-}
+    return{setMarker,board};
+})();
+
+//create player object:
+const players = (function (){
+
+    let playerOne = {
+        playerName : "chichi",
+        marker: "X"
+
+    };
+
+    let playerTwo = {
+        playerName : "chiko",
+        marker: "O"
+
+    };
+
+    return {playerOne,playerTwo};
+})();
 
 //create object to control game flow:
-function GameController(){
-    
+function gameController (){
+    //create a variable to contain the total number of X's and O's on the board 
+    let count = 0;
+    //create function that switches players based on number of Markers on the board
+    function selectActivePlayer (){
+        //loop to calculate number of X's and O's on the board
+        for (let i = 0; i < 3; i++){
+           gameBoard.board[i].forEach(value=>{
+     
+               if(value == 'X' || value == "O"){
+                  count++; 
+               }
+     
+           }
+             )
+        }
+        //condition that returns player two marker if board is empty or
+        //if board has odd total number of markers and returns player one marker
+        //if board has even number of markers
+        if (count % 2 != 0){
+            return players.playerOne.marker;
+        }
+
+        else{
+            return players.playerTwo.marker;
+
+        }
+        
+
+    }
+   
+   
+   return {selectActivePlayer};
 }
-
-let trial= gameBoard()
-trial.setMark(0,0,"o");
-trial.setMark(2,1,"o");
-trial.setMark(0,0,"x");
-trial.setMark(1,0,"x");
-
-
-
-
-
-
-console.log(trial.board);
-
-
 
